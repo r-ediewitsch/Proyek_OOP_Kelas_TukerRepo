0fb2d322fd6652495e9aeb91c0d26505e5b6d6be
Raddief <tioraddief@gmail.com>
2024-11-20 21:30:34 +0700

    run di file EndlessEnds

---
+++ Enemy.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Enemy
+{
+    public string Name { get; set; }
+    public float Health { get; set; }
+    public float Damage { get; set; }
+
+    public Enemy(string name, float health, float damage)
+    {
+        Name = name;
+        Health = health;
+        Damage = damage;
+    }
+
+    public void Attack(Character player)
+    {
+        Console.WriteLine($"{Name} attacks {player.CharacterName} for {Damage} damage.");
+        player.Health -= Damage;
+        Console.WriteLine($"{player.CharacterName} has {player.Health} health left.");
+    }
+}
